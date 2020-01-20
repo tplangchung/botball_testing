7b082fe394798f86dcc5f8706a9743f797e41403
tplangchung <32085192+tplangchung@users.noreply.github.com>
2020-01-20 11:00:44 -0600

    Add files via upload

---
+++ Blah.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class  Blah
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Do you like dogs?");
+		 Scanner scan= new Scanner(System.in);
+        String syed = scan.nextLine();
+  
+        if (syed.equals("yes"))
+        {
+            System.out.println("You got it");
+        } 
+    }
+}
+JOURNAL 26 - write down WHY YOU THINK == did not work for this program and .equals work. alan was here
